4c2c9abe8a2bfccd8c074256282b8e9972ba7929
Damir Zaripov <zaripov.damir92@mail.ru>
2023-04-06 21:59:33 +0300

    EggTimer created

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let eggTimes = [
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720,
+    ]
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleText: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        stopSound()
+        let hardness = sender.currentTitle!
+        
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleText.text = hardness
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] timer in
+            
+            if secondsPassed < totalTime {
+                secondsPassed += 1
+                let percentageProgress = Double(secondsPassed) / Double(totalTime)
+                progressBar.setProgress(Float(percentageProgress), animated: true)
+            }
+            else {
+                timer.invalidate()
+                playSound()
+                titleText.text = "Get it out!"
+                
+            }
+        }
+        
+        func playSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+        }
+        func stopSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.stop()
+        }
     }
-
-
 }
 
